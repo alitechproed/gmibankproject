9294aae1f75abdbb62794af64aad16231deaf0bd
Ozkan <tatar66@users.noreply.github.com>
2020-11-04 21:41:02 +0100

    US_01

---
+++ src/test/java/gmibank/stepdefinitions/Hooks.java
@@ -0,0 +1,33 @@
+package gmibank.stepdefinitions;
+
+import gmibank.utilities.Driver;
+import io.cucumber.java.Scenario;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+    protected RequestSpecification spec01;
+
+    @Before
+    public void setup01() {
+        // Spec for API
+        spec01 = new RequestSpecBuilder().
+                setBaseUri("https://**************").
+                build();
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        // Take a screenshot
+        final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        if (scenario.isFailed()) {
+            scenario.embed(screenshot, "image/png");
+        }
+        Driver.closeDriver();
+    }
+}
