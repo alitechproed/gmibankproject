a6d82718bd1af852f4d46f8ef03b5e6c527b466d
Ozkan <tatar66@users.noreply.github.com>
2020-11-06 23:23:35 +0100

    us 01 ui test

--- src/test/java/gmibank/pages/US_01_Page.java
+++ src/test/java/gmibank/pages/US_01_Page.java
@@ -34,6 +34,33 @@ public class US_01_Page {
     @FindBy(xpath = "//div[.='Your Last Name is required']")
     public WebElement lastnameHataMesaji;
 
+    @FindBy(id = "address")
+    public WebElement addressTextbox;
+
+    @FindBy(id = "mobilephone")
+    public WebElement mobilePhoneTextbox;
+
+    @FindBy(xpath = "//div[.='Your mobile phone number is invalid']")
+    public WebElement mobilePhoneHataMesaji;
+
+    @FindBy(id = "username")
+    public WebElement usernameTextbox;
+
+    @FindBy(id = "email")
+    public WebElement emailTextbox;
+
+    @FindBy(id = "firstPassword")
+    public WebElement firstPasswordTextbox;
+
+    @FindBy(id = "secondPassword")
+    public WebElement secondPasswordTextbox;
+
+    @FindBy(id = "register-submit")
+    public WebElement registerButton;
+
+    @FindBy(xpath = "//strong[.='Registration saved!']")
+    public WebElement registrationSuccessMessage;
+
 
 
 
