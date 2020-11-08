64836a51667a6b62a44a0b4873a745c7e1ff275e
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2020-11-08 23:51:57 +0100

    08.11 samet localden remota

--- src/test/java/gmibank/pages/US_10_Page.java
+++ src/test/java/gmibank/pages/US_10_Page.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class US10Page {
-    public US10Page (){
+public class US_10_Page {
+    public US_10_Page(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
