7e3f6f5d1616ffb1afad446d7e9a4285bbc1ec52
Saltuk <saltukarkali@gmail.com>
2020-11-15 11:25:06 +0100

    ilk Sprint son commit local saltuk'dan 15112020

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "gmibank/stepdefinitions",
-        tags = "@TC1401",
+        tags = "",
         dryRun = false
 
 )
