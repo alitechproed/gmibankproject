ed9c1b9903c14283b922236e4595764f2a98e965
birngarten <67049672+birngarten@users.noreply.github.com>
2020-11-15 13:24:39 +0100

    Update Runner.java
    glue ve tags arasindaki bir satirlik bosluk giderildi

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -13,7 +13,6 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "gmibank/stepdefinitions",
-
         tags = "@SmokeTest",
         dryRun = false
 
