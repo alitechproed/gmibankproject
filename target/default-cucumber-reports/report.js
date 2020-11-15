cc1a776a86dbc7654f2ab5a808702193aaa4c9b2
birngarten <resitozyurt@gmail.com>
2020-11-15 15:11:57 +0100

    Merge branch 'resit' of C:\Users\Administrator\IdeaProjects\gmibankproject with conflicts.

---
+++ target/default-cucumber-reports/report.js
@@ -0,0 +1,363 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/us_07.feature");
+formatter.feature({
+  "name": "US_07 Test",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@US_07"
+    },
+    {
+      "name": "@SmokeTest"
+    }
+  ]
+});
+formatter.scenarioOutline({
+  "name": "TC_060 email test",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@TC_0701"
+    }
+  ]
+});
+formatter.step({
+  "name": "Kullanici email textBoxina tiklar",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "Kullanici \"\u003cemail\u003e\" girer",
+  "keyword": "And "
+});
+formatter.step({
+  "name": "kullanici hatali email  girdiginde hata mesajini gorur",
+  "keyword": "Then "
+});
+formatter.examples({
+  "name": "invalid emailler",
+  "description": "",
+  "keyword": "Examples",
+  "rows": [
+    {
+      "cells": [
+        "email"
+      ]
+    },
+    {
+      "cells": [
+        "abc122afhmail.com"
+      ]
+    },
+    {
+      "cells": [
+        "abc122afh@mail"
+      ]
+    }
+  ]
+});
+formatter.background({
+  "name": "Kullanici bir user olarak gmibank anasayfasina gider",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "Kullanici bir user olarak gmibank anasayfasina gider",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici bir user olarak sag ust kosede bulunan insan simgesine tiklar",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici bir user olarak acilan menuden signIn linkine tiklar",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici username textboxina \"AAbc398\" girer",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici  password textboxina \"A1b2C3\" girer",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici signin butonuna tiklar",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici sag ustteki insan simgesine tekrar tiklar ve userInfo basligini secer",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "TC_060 email test",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@US_07"
+    },
+    {
+      "name": "@SmokeTest"
+    },
+    {
+      "name": "@TC_0701"
+    }
+  ]
+});
+formatter.step({
+  "name": "Kullanici email textBoxina tiklar",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici \"abc122afhmail.com\" girer",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "kullanici hatali email  girdiginde hata mesajini gorur",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "Kullanici bir user olarak gmibank anasayfasina gider",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "Kullanici bir user olarak gmibank anasayfasina gider",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici bir user olarak sag ust kosede bulunan insan simgesine tiklar",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici bir user olarak acilan menuden signIn linkine tiklar",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici username textboxina \"AAbc398\" girer",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici  password textboxina \"A1b2C3\" girer",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici signin butonuna tiklar",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici sag ustteki insan simgesine tekrar tiklar ve userInfo basligini secer",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "TC_060 email test",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@US_07"
+    },
+    {
+      "name": "@SmokeTest"
+    },
+    {
+      "name": "@TC_0701"
+    }
+  ]
+});
+formatter.step({
+  "name": "Kullanici email textBoxina tiklar",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici \"abc122afh@mail\" girer",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "kullanici hatali email  girdiginde hata mesajini gorur",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.background({
+  "name": "Kullanici bir user olarak gmibank anasayfasina gider",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "Kullanici bir user olarak gmibank anasayfasina gider",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici bir user olarak sag ust kosede bulunan insan simgesine tiklar",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici bir user olarak acilan menuden signIn linkine tiklar",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici username textboxina \"AAbc398\" girer",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici  password textboxina \"A1b2C3\" girer",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici signin butonuna tiklar",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici sag ustteki insan simgesine tekrar tiklar ve userInfo basligini secer",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "TC_061 language  test",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@US_07"
+    },
+    {
+      "name": "@SmokeTest"
+    },
+    {
+      "name": "@TC_0702"
+    }
+  ]
+});
+formatter.step({
+  "name": "Kullanici language  secenegine tiklar",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "Kullanici language dropDown\u0027unun icinde sadece  \"English\" veya \"Türkçe\" dilleri oldugunu gorur",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.after({
+  "status": "passed"
+});
+});
