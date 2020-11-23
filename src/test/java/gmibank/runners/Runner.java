0ff5394817869b8c12dc0f4faec7c02768cdfcd4
birngarten <resitozyurt@gmail.com>
2020-11-23 15:49:48 +0100

    US_23 from resit local to remote branch

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -13,9 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "gmibank/stepdefinitions",
-
-        tags = "@US_23",
-
+        tags = "",
 
         dryRun = false
 
