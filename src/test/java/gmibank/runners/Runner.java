7fb9363cfe0593d3a47c7dd4f0bab0fde714efd0
birngarten <67049672+birngarten@users.noreply.github.com>
2020-11-23 15:47:36 +0100

    Update Runner.java

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -13,9 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "gmibank/stepdefinitions",
-
-        tags = " @TC_2101",
-
+        tags = "",
 
         dryRun = false
 
